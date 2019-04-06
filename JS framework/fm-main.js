905b127825d71623be653db091a0adb440403ca4
lthoedt <38183200+lthoedt@users.noreply.github.com>
2019-04-06 16:51:22 +0200

    Some more tweaking

--- JS framework/fm-main.js
+++ JS framework/fm-main.js
@@ -3,7 +3,7 @@
 // You need to have jquery for this file to work.
 
 $(".fm-input_nav").focus(function(e) {
-    $(this).parent().css({backgroundColor: "white", transition: "all 200ms"});
+    $(this).parent().css({backgroundColor: "white", color: "black", transition: "all 200ms"});
     console.log("checl");
 }).blur(function () {
     $(this).parent().removeAttr('style');
@@ -12,4 +12,16 @@ $(".fm-input_nav").focus(function(e) {
 $(".fm-navbar_menu").click(function() {
     $(".fm-navbar_wrapper").toggleClass("responsive");
     console.log("check");
+})
+
+$(".fm-dropdown_btn").click(function(){
+    if ($(".fm-navbar_dropdown").css("display") == "block") {
+        $(".fm-navbar_dropdown").css({display: "none"});
+    }else {
+        $(".fm-navbar_dropdown").css({display: "block"});
+    }
+})
+
+$(".fm-navbar_dropdown").mouseleave(function(){
+    $(".fm-navbar_dropdown").css({display: "none"});
 })
